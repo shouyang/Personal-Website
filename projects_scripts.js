95d196a67bb3b080185d96b763bb4cd257ac2d14
shouyang <shouyang@ualberta.ca>
2016-02-03 19:30:26 -0700

    Project Categorization is now done!
    Added Content as well.

--- projects_scripts.js
+++ projects_scripts.js
@@ -1,22 +1,22 @@
 // This file requires jQuery
 
 function ShowcaseGeneral(){
-	$("#showcase_panel").load("project_general.html");
+	$("#showcase_panel").load("projectsub/project_general.html");
 	console.log("test function");
 	return true;
 }
 function ShowcaseCompSci(){
-	$("#showcase_panel").load("project_CompSci.html");
+	$("#showcase_panel").load("projectsub/project_CompSci.html");
 	console.log("test function");
 	return true;
 }
 function ShowcaseFinance(){
-	$("#showcase_panel").load("project_Finance.html");
+	$("#showcase_panel").load("projectsub/project_Finance.html");
 	console.log("test function");
 	return true;
 }
 function ShowcaseMilSci(){
-	$("#showcase_panel").load("project_MilSci.html");
+	$("#showcase_panel").load("projectsub/project_MilSci.html");
 	console.log("test function");
 	return true;
 }
