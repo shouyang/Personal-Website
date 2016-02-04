b56ea66e4b53a38f40e874ad4fe57653488a6edd
shouyang <shouyang@ualberta.ca>
2016-02-03 18:48:14 -0700

    Made Loadable Project Categories

--- projects_scripts.js
+++ projects_scripts.js
@@ -1,7 +1,22 @@
 // This file requires jQuery
 
-function test(){
-	document.getElementById("showcase_panel").innerHTML = "s";
+function ShowcaseGeneral(){
+	$("#showcase_panel").load("project_general.html");
+	console.log("test function");
+	return true;
+}
+function ShowcaseCompSci(){
+	$("#showcase_panel").load("project_CompSci.html");
+	console.log("test function");
+	return true;
+}
+function ShowcaseFinance(){
+	$("#showcase_panel").load("project_Finance.html");
+	console.log("test function");
+	return true;
+}
+function ShowcaseMilSci(){
+	$("#showcase_panel").load("project_MilSci.html");
 	console.log("test function");
 	return true;
 }
